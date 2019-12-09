78fadac5e6dd66612e3e9074091776087f9c8e6f
olivermchaleTU <olivermchale98@gmail.com>
2019-12-09 17:19:32 +0000

    Added address form/formatting to the purchase component

--- src/Components/PurchaseOrderPurchase.jsx
+++ src/Components/PurchaseOrderPurchase.jsx
@@ -12,7 +12,11 @@ import { connect } from 'react-redux';
 
 class PurchaseOrderPurchase extends React.Component {
     state = {
-        products: []
+        products: [],
+        quantity: 1,
+        address: '',
+        name: '',
+        postcode:  ''
     }
     render() {
         return (
@@ -20,19 +24,59 @@ class PurchaseOrderPurchase extends React.Component {
             <Card>
                 <Row className="mt">
                     <Col xs={12} md={4}>
-                        <Image src={"https://external-preview.redd.it/LddK4slWhSO6pNNkBs9_gYnnCnLIjAz3lIFOYR2Bzd4.jpg?auto=webp&s=9e8b51aa6fe8d153ad09f893e132ae5a37775729"}/>
+                        <Image className="placeholder-img" src={"https://external-preview.redd.it/LddK4slWhSO6pNNkBs9_gYnnCnLIjAz3lIFOYR2Bzd4.jpg?auto=webp&s=9e8b51aa6fe8d153ad09f893e132ae5a37775729"}/>
                     </Col>
-                    <Col xs={12} md={8}>
-                        <h1>Create Purchase Order</h1>
+                    <Col xs={12} md={4}>
+                        <h1>{this.props.name}</h1>
+                    </Col>
+                </Row>
+                <Form.Group as={Row}>
+                    <Col xs={9} md={{span: 4, offset: 4}}>
+                        <Form.Control 
+                            name="quantity" 
+                            onChange={this.handleInputChange} 
+                            placeholder="Quantity" />
+                    </Col>
+                </Form.Group>
+                <Row className="mt-15">
+                    <Col xs={12} md={{span: 4, offset: 4}}>
+                        <p>Price: {'£'+(this.props.price * this.state.quantity).toFixed(2)}</p>
+                    </Col>
+                    <Col xs={12} md={12}>
+                        <hr/>
+                    </Col>
+                </Row>
+                <Row className="mt-15">
+                    <Col className="mt-15" md={{span: 4, offset:4}} xs = {12}>
+                        <Form.Control 
+                            name="address" 
+                            type="text"
+                            onChange={this.handleInputChange} 
+                            placeholder="Address" />
+                    </Col>
+                    <Col className="mt-15" md={{span: 4, offset:4}} xs = {12}>
+                        <Form.Control 
+                            name="postcode" 
+                            type="text"
+                            onChange={this.handleInputChange} 
+                            placeholder="Postcode" />
+                    </Col>
+                    <Col className="mt-15 mb" md={{span: 4, offset:4}} xs = {12}>
+                        <Form.Control 
+                            name="name" 
+                            type="text"
+                            onChange={this.handleInputChange} 
+                            placeholder="Name" />
+                    </Col>
+                    <Col xs={12} md={12}>
+                        <hr/>
                     </Col>
                 </Row>
                 <Row>
-                    <Col md={{span: 4, offset:4}} sm={{span: 10, offset: 4}}>
+                    <Col md={{span: 4, offset:4}} sm={{span: 10, offset: 2}}>
                         <PaymentForm></PaymentForm>
                     </Col>
                 </Row>
-                <Form>
-                </Form>
             </Card>
             <button onClick={this.clicked}>btn</button>
         </Container>
@@ -42,6 +86,11 @@ class PurchaseOrderPurchase extends React.Component {
     clicked = () => {
         console.log(this.state); 
         console.log(this.props); //props holds our redux data now :)
+    }
+
+    handleInputChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
     }
 
 }
