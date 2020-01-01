0a4acff9a2a7dccc91a59d1a07d8264ee558b243
olivermchaleTU <olivermchale98@gmail.com>
2020-01-01 21:11:45 +0000

    Add security using OAuth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TeamANavBar from './Components/TeamANavBar';
@@ -11,47 +11,87 @@ import MockProducts from './Components/MockProducts';
 import PurchaseOrderPurchase from './Components/PurchaseOrderPurchase';
 import OrdersTable from './Components/OrdersTable';
 import OrderDetails from './Components/OrderDetails';
+import Cookies from 'universal-cookie';
+import oauth from 'axios-oauth-client';
+import axios from 'axios';
+import {
+  authUrl,
+  username,
+  password,
+  clientID,
+  clientSecret,
+  clientScope
+} from './Components/Constants';
 
-function App() {
-  return (
-    <Router>
-    <React.Fragment>
-      <TeamANavBar/>
-    </React.Fragment>
-    <Route 
-      exact 
-      path='/users'
-      component={AccountTable}/>
-    <Route
-      exact
-      path='/users/:id'
-      component={UserProfile}/>
-    <Route
-      exact
-      path='/users/edit/:id'
-      component={EditUserProfile}/>
-    <Route
-      exact
-      path='/products/'
-      component={MockProducts}/>
-    <Route
-      exact
-      path='/purchaseorders/products/:ean'
-      component={ViewProducts}/>
-    <Route
-      exact
-      path='/purchaseorders/products/purchase/:id'
-      component={PurchaseOrderPurchase}/>
-    <Route
-      exact
-      path='/purchaseorders/'
-      component={OrdersTable}/>
-    <Route
-      exact
-      path='/purchaseorders/:id'
-      component={OrderDetails}/>
-    </Router>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.token = (new Cookies()).get("AccessToken");
+    axios.defaults.headers.common['Authorization'] = "Bearer " + this.token;
+    axios.defaults.headers.common['Content-Type'] = 'application/json';
+    axios.defaults.headers.common['Content-Type'] = 'application/x-www-form-urlencoded';
+  }
+
+  render() {
+    return (
+      <Router>
+      <React.Fragment>
+        <TeamANavBar/>
+      </React.Fragment>
+      <Route 
+        exact 
+        path='/users'
+        component={AccountTable}/>
+      <Route
+        exact
+        path='/users/:id'
+        component={UserProfile}/>
+      <Route
+        exact
+        path='/users/edit/:id'
+        component={EditUserProfile}/>
+      <Route
+        exact
+        path='/products/'
+        component={MockProducts}/>
+      <Route
+        exact
+        path='/purchaseorders/products/:ean'
+        component={ViewProducts}/>
+      <Route
+        exact
+        path='/purchaseorders/products/purchase/:id'
+        component={PurchaseOrderPurchase}/>
+      <Route
+        exact
+        path='/purchaseorders/'
+        component={OrdersTable}/>
+      <Route
+        exact
+        path='/purchaseorders/:id'
+        component={OrderDetails}/>
+      </Router>
+    );
+  }
+
+  componentWillMount() {
+    const getToken = oauth.client(axios.create(), {
+      url: authUrl,
+      grant_type: 'password',
+      client_id: clientID,
+      client_secret: clientSecret,
+      username: username,
+      password: password,
+      scope: clientScope
+    });
+
+    getToken().then(resp => {
+      document.cookie = `AccessToken=${resp.access_token}`;
+    })
+
+  }
+
 }
 
 export default App;
---
+++ src/Components/Constants.js
@@ -0,0 +1,11 @@
+export const authUrl = "https://threeamigosauth.azurewebsites.net/connect/token";
+                          
+export const username = "admin@example.com";
+
+export const password = ".Password123";
+
+export const clientID = "threeamigos_app";
+
+export const clientSecret = "secret";
+
+export const clientScope = "thamco_account_api staff_api";
